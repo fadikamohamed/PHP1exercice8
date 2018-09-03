42cd578acd5061f287d82c7951f6152a244aee41
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 14:44:35 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 8</title>
+  </head>
+  <body>
+    <?php
+      $addition = 3 + 4;
+      $multipllication = 5 * 20;
+      $division = 45 / 5;
+
+    echo $addition . ' ' . $multipllication . ' ' . $division;
+     ?>
+  </body>
+</html>
